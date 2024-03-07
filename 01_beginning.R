9da4ff24e50e570f80018249e15015a521578f2f
Vid-Stipcevic <162352301+Vid-Stipcevic@users.noreply.github.com>
2024-03-07 17:05:15 +0100

    Create 01_beginning.R

---
+++ 01_beginning.R
@@ -0,0 +1,15 @@
+# Commandi per ricevere 3217833:
+a<-14*229845
+b<-2+1
+a+b
+
+# Arrays
+flowers<-c(3, 6, 8, 10, 15, 18)
+flowers
+
+insects<-c(10, 16, 25, 42, 61, 73)
+insects
+
+# Plots (X, Y, Symbol type, Symbol size, Symbol color)
+plot(flowers, insects, pch=8, cex=4, col="darkgreen")
+
