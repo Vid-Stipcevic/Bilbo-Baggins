b14b1c8188f4bd04b93d4e8e51e771f8861daedf
Vid-Stipcevic <162352301+Vid-Stipcevic@users.noreply.github.com>
2024-04-08 09:59:51 +0200

    Update 04_Classification.R

--- 04_Classification.R
+++ 04_Classification.R
@@ -6,12 +6,22 @@ library(terra)
 library(imageRy)
 library(ggplot2)
 
-# 
+sun<-im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 
+# Image classification:
 
+sunc<-im.classify(sun, num_clusters = 3)
 
+m1992<-im.import("matogrosso_l5_1992219_lrg.jpg")
+m2006<-im.import("matogrosso_ast_2006209_lrg.jpg")
 
+m1992c<-im.classify(m1992, num_clusters = 2)
 
+# m1992 : class 1 = human, class 2 = forest
+
+m2006c<-im.classify(m2006, num_clusters = 2)
+
+# m2006 : class 1 = forest, class 2 = human
 
 
 
