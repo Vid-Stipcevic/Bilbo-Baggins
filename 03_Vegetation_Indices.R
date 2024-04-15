0e0cd5481ba6b9f3b0bc02c02e508dbc53827cec
Vid-Stipcevic <162352301+Vid-Stipcevic@users.noreply.github.com>
2024-04-15 11:55:55 +0200

    Update 03_Vegetation_Indices.R

--- 03_Vegetation_Indices.R
+++ 03_Vegetation_Indices.R
@@ -12,7 +12,6 @@ im.plotRGB(m1992, 1, 2, 3)
 im.plotRGB(m1992, r=2, g=1, b=3)
 im.plotRGB(m1992, r=2, g=3, b=1)
 
-# import the recent image
 m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
 im.plotRGB(m2006, r=2, g=3, b=1)
 
@@ -27,51 +26,32 @@ im.plotRGB(m2006, r=2, g=3, b=1)
 dvi1992 = m1992[[1]] - m1992[[2]]
 plot(dvi1992)
 
-cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
+cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100) # specifying a color scheme
 plot(dvi1992, col=cl)
 
 # exercise: calculate dvi of 2006
 dvi2006 = m2006[[1]] - m2006[[2]]
 plot(dvi2006, col=cl)
 
-# NDVI (Normalized DVI)
+# NDVI (Normalized DVI, always from -1 to 1)
 ndvi1992 = (m1992[[1]] - m1992[[2]]) / (m1992[[1]] + m1992[[2]])
 ndvi1992 = dvi1992 / (m1992[[1]] + m1992[[2]])
 plot(ndvi1992, col=cl)
 
-# NDVI
 ndvi2006 = dvi2006 / (m2006[[1]] + m2006[[2]])
 plot(ndvi2006, col=cl)
 
-# par
-par(mfrow=c(1,2))
-plot(ndvi1992, col=cl)
-plot(ndvi2006, col=cl)
-
-clvir <- colorRampPalette(c("violet", "dark blue", "blue", "green", "yellow"))(100) # specifying a color scheme
+clvir<-colorRampPalette(c("violet", "dark blue", "blue", "green", "yellow"))(100)
 par(mfrow=c(1,2))
 plot(ndvi1992, col=clvir)
 plot(ndvi2006, col=clvir)
 
-# speediing up calculation
+# speeding up calculation
 ndvi2006a <- im.ndvi(m2006, 1, 2)
 plot(ndvi2006a, col=cl)
 
-# DVI calculation
-
-dvi1002 = m1992[[1]] - m1992[[2]]
-# or:   dvi1002 = m1992$matogrosso~2219_lrg_1 - m1992$matogrosso~2219_lrg_2
-
-dvi2006 = m2006[[1]] - m2006[[2]]
-
-# example: 
-par(mfrow=c(1,2))
-plot(dvi1992, col=cl, main="1992")
-plot(dvi2006, col=cl, main="2006")
-
-# NDVI: always from -1 to 1
-ndvi1992 = dvi1992 / (m1992[[1]]+m1992[[2]])
-ndvi2006 = dvi2006 / (m2006[[1]]+m2006[[2]])
+# alternative DVI calculation:
+dvi1002 = m1992$matogrosso~2219_lrg_1 - m1992$matogrosso~2219_lrg_2
 
 
 
