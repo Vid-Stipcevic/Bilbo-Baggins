f7ca8738475c00477fedcf7bf8b8b9c9d1204e7d
Vid-Stipcevic <162352301+Vid-Stipcevic@users.noreply.github.com>
2024-04-23 17:04:30 +0200

    Update 06_Importing_external_data.R

--- 06_Importing_external_data.R
+++ 06_Importing_external_data.R
@@ -6,13 +6,16 @@ library(RNetCDF)
 library(imageRy)
 library(terra)
 
+# Example of image sources: "https://github.com/ducciorocchini/telerilevamento2024/blob/main/Code/07_data_sources.md"
+
 setwd() # Sets a working directory
-setwd("C:/Users/vidst/Downloads") # Note: transform the backlashes "\" into slashes "/"
+setwd("C:/Users/.../Downloads") # Note: transform the backlashes "\" into slashes "/"
 
 eclissi<-rast("eclissi.png")
 im.plotRGB(eclissi,1,2,3)
 
-
+dif=eclissi[[1]]-eclissi[[2]]
+plot(dif)
 
 
 
