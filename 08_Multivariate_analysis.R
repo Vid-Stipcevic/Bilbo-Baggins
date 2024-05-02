9ccc852d54a4aaac19f7154f70e4d9cd5d9751fc
Vid-Stipcevic <162352301+Vid-Stipcevic@users.noreply.github.com>
2024-05-02 17:47:37 +0200

    Update 08_Multivariate_analysis.R

--- 08_Multivariate_analysis.R
+++ 08_Multivariate_analysis.R
@@ -16,7 +16,13 @@ im.plotRGB(sentdo,r=4,g=3,b=2)
 
 pairs(sentdo)
 
-# 
+# pca
 
+pcimage<-im.pca(sentdo)
 
+tot<-sum(1463.82589, 570.28864, 44.84740, 29.57957)
+tot
+1463.82589*100/tot # Percentage of component range variance explained
 
+vir<-colorRampPalette(viridis(7))(100)
+plot(pcimage, col=vir)
